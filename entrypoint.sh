52b626e24ffe520a8381136b262f5e9222f46c46
Mike Hawke <73037912+MikeHawkeCF@users.noreply.github.com>
2021-03-10 12:15:43 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # OxideMod has been replaced with uMod
-    echo "Updating uMod..."
-    umod update -P
-    echo "Done updating uMod!"
+ #   echo "Updating uMod..."
+  #  umod update -P
+   # echo "Done updating uMod!"
 
 # Fix for Rust not starting
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
